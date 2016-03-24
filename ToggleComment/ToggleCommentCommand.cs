fae492b84e34e4f5d1b3f32fe486285047874b43
munyabe <yo.betek@gmail.com>
2016-03-24 13:10:43 +0900

    Implement a command to comment/uncomment the selected lines.

--- ToggleComment/ToggleCommentCommand.cs
+++ ToggleComment/ToggleCommentCommand.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 
 namespace ToggleComment
@@ -12,6 +16,16 @@ namespace ToggleComment
         /// コマンドのIDです。
         /// </summary>
         public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// 選択された行をコメントアウトするコマンドです。
+        /// </summary>
+        private const string COMMENT_SELECTION_COMMAND = "Edit.CommentSelection";
+
+        /// <summary>
+        /// 選択された行のコメントを解除するコマンドです。
+        /// </summary>
+        private const string UNCOMMENT_SELECTION_COMMAND = "Edit.UncommentSelection";
 
         /// <summary>
         /// コマンドメニューグループのIDです。
@@ -43,6 +57,33 @@ namespace ToggleComment
         /// <inheritdoc />
         protected override void Execute(object sender, EventArgs e)
         {
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            var textDocument = dte.ActiveDocument.Object("TextDocument") as TextDocument;
+            if (textDocument != null)
+            {
+                var selection = textDocument.Selection;
+                SelectLines(selection);
+
+                var regex = new Regex(@"^[\s]*//");
+                var isAllComment = selection.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                    .Where(x => string.IsNullOrWhiteSpace(x) == false)
+                    .All(regex.IsMatch);
+
+                var command = isAllComment ? UNCOMMENT_SELECTION_COMMAND : COMMENT_SELECTION_COMMAND;
+                dte.ExecuteCommand(command);
+            }
+        }
+
+        /// <summary>
+        /// 選択中の行を行選択状態にします。
+        /// </summary>
+        private static void SelectLines(TextSelection selection)
+        {
+            var bottom = selection.BottomLine;
+
+            selection.MoveToDisplayColumn(selection.TopLine, 1, false);
+            selection.MoveToDisplayColumn(bottom, 0, true);
+            selection.EndOfLine(true);
         }
     }
 }
