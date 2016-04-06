e90af248742b55ac169ef3171bb7edcc3a44a9a9
munyabe <yo.betek@gmail.com>
2016-04-06 12:16:11 +0900

    Add a test project.

--- ToggleComment/Properties/AssemblyInfo.cs
+++ ToggleComment/Properties/AssemblyInfo.cs
@@ -31,3 +31,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: InternalsVisibleTo("Test.ToggleComment, PublicKey=002400000480000094000000060200000024000052534131000400000100010047b96a0f06c95f73dac6c69f6846bab6b23f2d6038ad506027fac7035e48e0b04c52dbac55ad613fae7694cdb7066341c10c577a01299693acd45bd956e133cdc82c303f9041aec3d6a9309ed8294197c20718f62b5f3bdacf958420d8b6169844e6f52d47934d2624173e23d8ec673ddc1b2c779e4dd9b336cb5a534eaf1ecc")]
+#endif
